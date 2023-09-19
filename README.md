56bb66960ddae2014bfb026e5cc43b1adc153443
Magnus Stubman <magnus.stubman@mandiant.com>
2023-09-19 14:43:16 +0200

    Add DEF generation functionality to exports.py

--- README.md
+++ README.md
@@ -99,6 +99,30 @@ architecture: 64-bit
 +----------------------------+--------------------------------+------------------+-----------+
 ```
 
+Or generate a DEF file manually for individual files:
+
+```
+$ ./exports.py version.dll -d 'C:\Windows\System32\version.dll'
+EXPORTS
+  GetFileVersionInfoA="C:\Windows\System32\version".GetFileVersionInfoA
+  GetFileVersionInfoByHandle="C:\Windows\System32\version".GetFileVersionInfoByHandle
+  GetFileVersionInfoExA="C:\Windows\System32\version".GetFileVersionInfoExA
+  GetFileVersionInfoExW="C:\Windows\System32\version".GetFileVersionInfoExW
+  GetFileVersionInfoSizeA="C:\Windows\System32\version".GetFileVersionInfoSizeA
+  GetFileVersionInfoSizeExA="C:\Windows\System32\version".GetFileVersionInfoSizeExA
+  GetFileVersionInfoSizeExW="C:\Windows\System32\version".GetFileVersionInfoSizeExW
+  GetFileVersionInfoSizeW="C:\Windows\System32\version".GetFileVersionInfoSizeW
+  GetFileVersionInfoW="C:\Windows\System32\version".GetFileVersionInfoW
+  VerFindFileA="C:\Windows\System32\version".VerFindFileA
+  VerFindFileW="C:\Windows\System32\version".VerFindFileW
+  VerInstallFileA="C:\Windows\System32\version".VerInstallFileA
+  VerInstallFileW="C:\Windows\System32\version".VerInstallFileW
+  VerLanguageNameA="C:\Windows\System32\version".VerLanguageNameA
+  VerLanguageNameW="C:\Windows\System32\version".VerLanguageNameW
+  VerQueryValueA="C:\Windows\System32\version".VerQueryValueA
+  VerQueryValueW="C:\Windows\System32\version".VerQueryValueW
+```
+
 
 #### References
 
--- exports.py
+++ exports.py
@@ -4,52 +4,80 @@ import sys
 import glob
 import json
 import pefile
+import argparse
 
 from prettytable import PrettyTable
 
+parser = argparse.ArgumentParser(description='Tool to show exports of a given DLL.')
 
-el = sys.argv[1]
-print('binary: ' + el)
+parser.add_argument('filename', help='filename of binary to analyse',type=argparse.FileType('r'))
+#parser.add_argument('-d', '--deff', help='Output in DEF format instead of pretty table', required=False, action='store_true')
+parser.add_argument("-d", "--deff", metavar='<forwardtarget>', help="Instead of pretty table, print a DEF file with exports to a given target", required=False, action='store', default=None, type=str)
 
-pe = pefile.PE(el)
+args = parser.parse_args()
 
-imageBase = pe.OPTIONAL_HEADER.ImageBase
 
-print('image base: ' + '0x' + hex(imageBase)[2:].zfill(16) + ' (' + str(imageBase) + ')')
 
-is32bit = hex(pe.FILE_HEADER.Machine) == '0x14c'
+def printTable(pe):
+  print('binary: ' + args.filename.name)
 
-if is32bit:
-  print('architecture: 32-bit')
-else:
-  print('architecture: 64-bit')
+  imageBase = pe.OPTIONAL_HEADER.ImageBase
 
-d = [pefile.DIRECTORY_ENTRY["IMAGE_DIRECTORY_ENTRY_EXPORT"]]
+  print('image base: ' + '0x' + hex(imageBase)[2:].zfill(16) + ' (' + str(imageBase) + ')')
 
-pe.parse_data_directories(directories=d)
-exports = [(e) for e in pe.DIRECTORY_ENTRY_EXPORT.symbols]
-
-x = PrettyTable()
-x.field_names = ['name', 'address (imagebase + relative)', 'relative address', 'ordinal']
-
-for n in x.field_names:
-  x.align[n] = "l"
-
-for e in exports:
+  is32bit = hex(pe.FILE_HEADER.Machine) == '0x14c'
 
   if is32bit:
-    address = '0x' + hex(imageBase + e.address)[2:].zfill(8)
+    print('architecture: 32-bit')
   else:
-    address = '0x' + hex(imageBase + e.address)[2:].zfill(16)
+    print('architecture: 64-bit')
 
-  if e.forwarder:
-    address = str(e.forwarder.decode())
+  d = [pefile.DIRECTORY_ENTRY["IMAGE_DIRECTORY_ENTRY_EXPORT"]]
 
-  name = ''
+  pe.parse_data_directories(directories=d)
+  exports = [(e) for e in pe.DIRECTORY_ENTRY_EXPORT.symbols]
+  x = PrettyTable()
+  x.field_names = ['name', 'address (imagebase + relative)', 'relative address', 'ordinal']
 
-  if e.name:
-    name = str(e.name.decode())
+  for n in x.field_names:
+    x.align[n] = "l"
 
-  x.add_row([name, address, '0x' + hex(e.address)[2:].zfill(8), str(e.ordinal) + ' (' + hex(e.ordinal) + ')'])
+  for e in exports:
 
-print(x)
+    if is32bit:
+      address = '0x' + hex(imageBase + e.address)[2:].zfill(8)
+    else:
+      address = '0x' + hex(imageBase + e.address)[2:].zfill(16)
+
+    if e.forwarder:
+      address = str(e.forwarder.decode())
+
+    name = ''
+
+    if e.name:
+      name = str(e.name.decode())
+
+    x.add_row([name, address, '0x' + hex(e.address)[2:].zfill(8), str(e.ordinal) + ' (' + hex(e.ordinal) + ')'])
+
+  print(x)
+
+
+def printDeff(pe):
+  print('EXPORTS')
+
+  d = [pefile.DIRECTORY_ENTRY["IMAGE_DIRECTORY_ENTRY_EXPORT"]]
+  pe.parse_data_directories(directories=d)
+  exports = [(e) for e in pe.DIRECTORY_ENTRY_EXPORT.symbols]
+
+  for export in exports:
+    export = export.name.decode('utf-8')
+    print('  ' + export + '="' + args.deff[:-4] + '".' + export)
+
+
+
+pe = pefile.PE(args.filename.name)
+
+if args.deff:
+  printDeff(pe)
+else:
+  printTable(pe)
